de50756c9a7d804d6a90963fcb8d7f98eec1783c
Jozef Baas <jozefbaas@gmail.com>
2025-05-15 19:12:53 +0200

    add tables to database

---
+++ pdf-app/database/migrations/2025_05_15_170307_create_login_history_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('login_history', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->ipAddress('ip_address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable(); // štát
+            $table->string('user_agent')->nullable();
+            $table->timestamp('logged_in_at')->default(now());
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('login_history');
+    }
+};
