bfe786a1123fdbd8244f014bb8ffdd8a8bc36bd9
mclehotsky <mclehotsky@gmail.com>
2025-05-20 15:15:23 +0200

    updated url for db

--- api/database.py
+++ api/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import baseurl
 
-# Pripojenie cez meno kontajnera, nie localhost!
-DATABASE_URL = "mysql+pymysql://xbaas:12345jozef@127.0.0.1:3306/webtePDF"
+# Add baseurl.py, with the following content:
+# URL = "mysql+pymysql://username:password@127.0.0.1:3306/tablename"
+DATABASE_URL = baseurl.URL
 
 # Vytvorenie engine a session
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
